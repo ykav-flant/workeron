4d48e5187c0cf4b4b4a290dc23d248219d8f8097
Yaroslav Kavokin <yaroslav.kavokin@flant.com>
2024-11-21 20:32:28 +0300

    Init

---
+++ workeron.el
@@ -0,0 +1,29 @@
+;;; workeron.el  -*- lexical-binding: t; -*-
+;;
+;; Copyright (C) 2024 YK
+;;
+;; Author: YK
+;; Maintainer: YK <yk@gmail.com>
+;; Created: November 21, 2024
+;; Modified: November 21, 2024
+;; Version: 0.0.1
+;; Keywords: work
+;; Homepage: https://github.com/ykav/workeron
+;; Package-Requires: ((emacs "24.3"))
+;;
+;; This file is not part of GNU Emacs.
+;;
+;;; Commentary:
+;;
+;; Test. 1,2,1,2...
+;;
+;;; Code:
+
+
+(defun workeron--hello-world ()
+  "Print a hello world message."
+  (interactive)
+  (message "Hello, World!"))
+
+(provide 'workeron)
+;;; workeron.el ends here
